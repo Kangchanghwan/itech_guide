ed694424ae6a5776c4cc1b8e636a502cb25c5e31
CokeBear <43844779+Kangchanghwan@users.noreply.github.com>
2022-06-30 17:02:05 +0900

    pageable 설정

---
+++ src/main/java/com/itech/guide/global/log/LogAspect.java
@@ -0,0 +1,39 @@
+package com.itech.guide.global.log;
+
+import io.sentry.Sentry;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.aspectj.lang.ProceedingJoinPoint;
+import org.aspectj.lang.annotation.Around;
+import org.aspectj.lang.annotation.Aspect;
+import org.springframework.stereotype.Component;
+
+import java.util.Arrays;
+
+@Aspect
+@Slf4j
+@RequiredArgsConstructor
+@Component
+public class LogAspect {
+
+    private final LogTracer logTracer;
+
+    @Around("execution(* com.itech.guide..*Controller.*(..))||" +
+            "execution(* com.itech.guide..*Service.*(..))||" +
+            "execution(* com.itech.guide..*Repository.*(..))")
+    public Object doLog(ProceedingJoinPoint joinPoint) throws Throwable {
+        TraceStatusVO status = null;
+        boolean hasException = false;
+        try {
+            status = logTracer.begin("Return:"+joinPoint.getSignature().toString(), Arrays.deepToString(joinPoint.getArgs()));
+            return joinPoint.proceed();
+        } catch (Exception ex) {
+            logTracer.handleException(status, ex);
+            Sentry.captureMessage(ex.getMessage());
+            hasException = true;
+            throw ex;
+        } finally {
+            if(!hasException) logTracer.end(status);
+        }
+    }
+}
